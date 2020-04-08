fa9dc4cbf95e1e4e76daac3ace0f9b2cd30cc2d9
llseng <1300904522@qq.com>
2020-04-08 11:00:34 +0800

    apache+php

---
+++ httpd_php/download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 下载软件包
+wget -O ./packages/apr-1.7.0.tar.gz https://mirrors.koehn.com/apache//apr/apr-1.7.0.tar.gz
+wget -O ./packages/apr-util-1.6.1.tar.gz https://mirrors.koehn.com/apache//apr/apr-util-1.6.1.tar.gz
+wget -O ./packages/pcre-8.43.tar.gz https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz
+wget -O ./packages/httpd-2.4.43.tar.gz https://mirrors.koehn.com/apache//httpd/httpd-2.4.43.tar.gz
+wget -O ./packages/php-7.2.29.tar.gz https://www.php.net/distributions/php-7.2.29.tar.gz

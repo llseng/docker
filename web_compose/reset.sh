9150402045841f22b4bf6604e23350541deaf7f5
llseng <1300904522@qq.com>
2020-05-05 11:57:03 +0800

    Dockerfile 定制fpm镜像

--- web_compose/reset.sh
+++ web_compose/reset.sh
@@ -2,6 +2,9 @@
 
 #当前脚本目录
 BASEPATH=$(cd `dirname $0`; pwd)
-
+# 清除
 rm -rf ${BASEPATH}/{mysql,redis,nginx,fpm}/log/*
 rm -rf ${BASEPATH}/{mysql,redis}/data/*
+# 权限
+chmod -R 0777 ${BASEPATH}/{mysql,redis,nginx,fpm}/log
+chmod -R 0777 ${BASEPATH}/{mysql,redis}/data
